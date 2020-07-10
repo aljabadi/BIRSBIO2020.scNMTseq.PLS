3155bbab63129e3734e155f9f245c3a386230627
Al J Abadi <al.jal.abadi@gmail.com>
2020-07-10 17:41:46 +1000

    add RNA analysis

---
+++ R/utils.R
@@ -0,0 +1,49 @@
+## ----------- all_identical -----------
+## check if elements in a list are identical
+#' @export
+all_identical <- function(lst) {
+  for (j in seq_along(lst[-1])) {
+    if (!identical(lst[[j]], lst[[j+1]]))
+      stop(sprintf("not identical elements: %s and %s",j , j+1 ), call. = FALSE)
+  }
+  TRUE
+}
+
+## ----------- rbindListWithNames -----------
+## base::rbind a named list of data.frames adding a new column
+## indicating the name of the dataset in the list
+#' rbind a list of arrays
+#'
+#' @param lst A named list of arrays
+#' @param new_col Character, the name of the new column which adds the list names
+#' to the resulting array
+#'
+#' @name utils
+#' @export
+rbindListWithNames <- function(lst, new_col = "dataset") {
+  lst_with_newcol <- mapply(x=names(lst), y=lst, FUN = function(x, y){
+    y[,new_col] <- x
+    y
+  }, SIMPLIFY = FALSE)
+  Reduce(rbind, lst_with_newcol)
+}
+
+## ----------- named_list -----------
+## create a named list from a character vector
+## that can be used with apply family and
+## return a named list
+#' @export
+named_list <- function(char) {
+  out <- as.list(char)
+  names(out) <- char
+  out
+}
+
+## ----------- ggplot color hue -----------
+#' @export
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  grDevices::hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
+
